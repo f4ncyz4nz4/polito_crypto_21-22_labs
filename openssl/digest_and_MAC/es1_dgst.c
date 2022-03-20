a68babc5bc8b9c9bd5c24905aaf057c33517c53a
ippo <lorenzoippolito.99@gmail.com>
2022-03-20 11:57:27 +0100

    beautify code

--- openssl/digest_and_MAC/es1_dgst.c
+++ openssl/digest_and_MAC/es1_dgst.c
@@ -5,23 +5,27 @@
 
 #define BUFFSIZE 1024
 
-void handle_errors(){
+void handle_errors()
+{
   ERR_print_errors_fp(stderr);
   abort();
 }
 
-int main(int argc, char const **argv) {
+int main(int argc, char const **argv)
+{
   EVP_MD_CTX *md;
-  FILE* fd;
+  FILE *fd;
   unsigned int i, r_byte, md_byte;
   unsigned char buff[BUFFSIZE], md_buff[EVP_MD_size(EVP_sha1())];
 
-  if(argc!=2){
+  if (argc != 2)
+  {
     fprintf(stderr, "Too few arguments");
     abort();
   }
 
-  if((fd=fopen(argv[1], "r"))==NULL){
+  if ((fd = fopen(argv[1], "r")) == NULL)
+  {
     fprintf(stderr, "Error in opening file");
     abort();
   }
@@ -29,30 +33,36 @@ int main(int argc, char const **argv) {
   OpenSSL_add_all_algorithms();
   ERR_load_crypto_strings();
 
-  md=EVP_MD_CTX_new();
-  if(md==NULL){
+  md = EVP_MD_CTX_new();
+  if (md == NULL)
+  {
     fprintf(stderr, "Error allocate context");
     abort();
   }
 
-  if(!EVP_DigestInit(md, EVP_sha1())){
+  if (!EVP_DigestInit(md, EVP_sha1()))
+  {
     handle_errors();
   }
 
-  while((r_byte=fread(buff, 1, BUFFSIZE, fd))>0){
-    if(!EVP_DigestUpdate(md, buff, r_byte)){
+  while ((r_byte = fread(buff, 1, BUFFSIZE, fd)) > 0)
+  {
+    if (!EVP_DigestUpdate(md, buff, r_byte))
+    {
       handle_errors();
     }
   }
 
-  if(!EVP_DigestFinal(md, md_buff, &md_byte)){
+  if (!EVP_DigestFinal(md, md_buff, &md_byte))
+  {
     handle_errors();
   }
 
   EVP_MD_CTX_free(md);
 
   fprintf(stdout, "The hashed value is: ");
-  for (i = 0; i < md_byte; i++) {
+  for (i = 0; i < md_byte; i++)
+  {
     fprintf(stdout, "%02x", md_buff[i]);
   }
   fprintf(stdout, "\n");
